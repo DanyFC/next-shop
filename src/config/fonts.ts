3bae2bba4aa339d26d7c601bf1d24312797a26de
DanyFC <danyfarfanx@gmail.com>
2025-01-06 16:15:58 -0500

    feat: :tada: added assets and initial project configuration

---
+++ src/config/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Montserrat_Alternates } from "next/font/google";
+
+export const interFont = Inter({
+  weight: ["500", "700"]
+})
+
+export const montserrat = Montserrat_Alternates({
+  weight: ["500", "700"]
+})
