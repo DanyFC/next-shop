52e277e1f736eadfe2621126d6292a7d4a5aeb38
DanyFC <danyfarfanx@gmail.com>
2025-01-29 16:02:17 -0500

    feat: :passport_control: added auth next configuration with credentials provider and validation, added auth api route and server action to login

---
+++ src/auth/config.ts
@@ -0,0 +1,39 @@
+import bcrypt from 'bcrypt';
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { prisma } from "@/lib/prisma";
+import { signInSchema } from "@/lib/zod";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  pages: {
+    signIn: '/auth',
+    newUser: '/auth'
+  },
+  providers: [
+    CredentialsProvider({
+      id: 'local-credentials',
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email', placeholder: 'example@domain.com' },
+        password: { label: 'Password', type: 'password', placeholder: '●●●●●●●●' }
+      },
+      
+      async authorize(credentials) {
+        const parsedCredentials = signInSchema.safeParse(credentials)
+
+        if (!parsedCredentials.success) return null
+
+        const { email, password } = parsedCredentials.data
+        const user = await prisma.user.findUnique({
+          where: { email }
+        })
+
+        if (!user || !bcrypt.compareSync(password, user.password)) return null
+
+        const { password: _, ...rest } = user
+        return rest
+      }
+    })
+  ],
+})
