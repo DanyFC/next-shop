af35ed3daa855fdea57b92f65d5473d1c2391716
DanyFC <danyfarfanx@gmail.com>
2025-02-18 16:18:58 -0500

    feat: :sparkles: created cloudinary util to load images and updated the server action lo save images references in to database

--- src/actions/maintenance/create-update-product.ts
+++ src/actions/maintenance/create-update-product.ts
@@ -1,10 +1,12 @@
 "use server";
 
-import { Size } from "@/interfaces";
-import { prisma } from "@/lib/prisma";
-import { Gender } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
+
+import { Size } from "@/interfaces";
+import { prisma } from "@/lib/prisma";
+import { uploadImages } from "@/utils/cloudinary";
+import { Gender } from "@prisma/client";
 
 const productSchema = z.object({
   id: z.string().uuid().optional().nullable(),
@@ -25,16 +27,17 @@ export const createUpdateProduct = async (formData: FormData) => {
     const productParsed = productSchema.safeParse(data)
 
     if (!productParsed.success) throw new Error('Invalid data, verify the fields!')
-    
+
     const product = productParsed.data
     product.slug = product.slug.toLocaleLowerCase().replace(/ /g, '-').trim()
 
     const { id, ...rest } = product
     const prismaTx = await prisma.$transaction(async (tx) => {
       const tagsArray = rest.tags.split(',').map(tag => tag.trim().toLowerCase())
+      let productDB = null
 
       if (id) {
-        const updatedProduct = await tx.product.update({
+        productDB = await tx.product.update({
           where: { id },
           data: {
             ...rest,
@@ -46,29 +49,41 @@ export const createUpdateProduct = async (formData: FormData) => {
             }
           }
         })
-
-        return {
-          product: updatedProduct
-        }
+      } else {
+        productDB = await tx.product.create({
+          data: {
+            ...rest,
+            sizes: {
+              set: rest.sizes as Size[]
+            },
+            tags: {
+              set: tagsArray
+            }
+          }
+        })
       }
 
-      const savedProduct = await tx.product.create({
-        data: {
-          ...rest,
-          sizes: {
-            set: rest.sizes as Size[]
-          },
-          tags: {
-            set: tagsArray
-          }
+      if (!productDB) throw new Error('Error to save the product!')
+
+      // INFO: upload images
+      if (formData.getAll('images')) {
+        const { ok, data } = await uploadImages(formData.getAll('images') as File[])
+        if (!ok) {
+          throw new Error('Error to upload images!')
         }
-      })
+
+        await tx.productImage.createMany({
+          data: data!.map((image) => ({
+            url: image,
+            productId: productDB.id
+          }))
+        })
+      }
 
       return {
-        product: savedProduct
+        product
       }
     })
-
 
     revalidatePath('/maintenance/products')
     revalidatePath(`/maintenance/products/${prismaTx.product.slug}`)
---
+++ src/utils/cloudinary.ts
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { envConfig } from './config';
+
+const { cloudinayUrl } = envConfig()
+
+cloudinary.config(cloudinayUrl ?? '')
+
+export const uploadImages = async (images: File[]) => {
+  try {
+    const uploadPromises = images.map(async (image) => {
+      const buffer = await image.arrayBuffer()
+      const base64Image = Buffer.from(buffer).toString('base64')
+
+      return cloudinary.uploader.upload(`data:image/png;base64,${base64Image}`)
+        .then(res => res.secure_url)
+    })
+
+    const uploadedImages = await Promise.all(uploadPromises)
+
+    return {
+      ok:true,
+      data: uploadedImages
+    }
+  } catch (error) {
+    return {
+      ok: false,
+      message: 'Error uploading images!',
+    }
+  }
+}
